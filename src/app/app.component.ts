21542fc541149f6e7e57a04fbf0948df6a096b32
Curtis LayCraft <curtis.laycraft@zoho.com>
2018-10-13 10:09:09 -0700

    try automatic build from push again.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'A playground for web experiments!';
+  title = 'Automatic build failed lets try again!';
 }

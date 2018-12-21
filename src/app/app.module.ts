d441688401259d53945e7be58b8a94bf33b5366f
Curtis LayCraft <curtis.laycraft@zoho.com>
2018-12-20 22:33:32 -0800

    Make education list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { JobListComponent } from './job-list/job-list.component';
+import { EducationListComponent } from './education-list/education-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JobListComponent,
+    EducationListComponent,
   ],
   imports: [
     BrowserModule

d438e4c5af89f0131d5ae9abd2c90b1ecfa71352
Alice <alice@portable.local>
2019-01-13 12:59:45 -0800

    basic keyword parse

--- src/app/resume-builder/resume-builder.component.ts
+++ src/app/resume-builder/resume-builder.component.ts
@@ -6,12 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resume-builder.component.css']
 })
 export class ResumeBuilderComponent implements OnInit {
-  keywords: any;
+  keywords: string[];
   constructor() { }
 
   ngOnInit() {
   }
-  cleanKeywords(uncheckedUserInput){
-    this.keywords = uncheckedUserInput;
-  }
+  cleanKeywords(uncheckedUserInput:string){
+    //this is a basic chop of the keywords into a list
+    //split on commas and semicolons
+    this.keywords = uncheckedUserInput
+      .toLowerCase()
+      .split(",")
+      .map(item=>{
+        //clean whitespace off of the ends
+        return item.trim();
+      });
+  } 
 }

60f8e09851f3c90af86b1733dc3d7fa5d90bd3e8
Alice <alice@portable.local>
2019-01-13 12:05:09 -0800

    simplify structure by moving components into parent resume component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { JobListComponent } from './job-list/job-list.component';
 import { EducationListComponent } from './education-list/education-list.component';
 import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { ResumeBuilderComponent } from './resume-builder/resume-builder.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
     JobListComponent,
     EducationListComponent,
     TopNavBarComponent,
+    ResumeBuilderComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/resume-builder/resume-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-builder',
+  templateUrl: './resume-builder.component.html',
+  styleUrls: ['./resume-builder.component.css']
+})
+export class ResumeBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

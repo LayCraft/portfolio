d558dc40653f764cca850841f2811851acc5b77e
Curtis <curtis.laycraft@zoho.com>
2019-07-23 11:01:37 -0700

    Make checkboxes for option areas

--- src/app/resume-builder/resume-builder.component.ts
+++ src/app/resume-builder/resume-builder.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-resume-builder',
-  templateUrl: './resume-builder.component.html',
-  styleUrls: ['./resume-builder.component.css']
+	selector: 'app-resume-builder',
+	templateUrl: './resume-builder.component.html',
+	styleUrls: ['./resume-builder.component.css']
 })
 export class ResumeBuilderComponent implements OnInit {
-  keywords: string[];
-  constructor() { }
+	keywords: string[];
+	showObjective = true;
+	showHighlights = true;
+	showEducation = true;
+	showExperience = true;
+	constructor() { }
 
-  ngOnInit() {
-  }
-  cleanKeywords(uncheckedUserInput: string) {
-    // this is a basic chop of the keywords into a list
-    // split on commas and semicolons
-    this.keywords = uncheckedUserInput
-      .split(',')
-      .map(item => {
-        // clean whitespace off of the ends
-        return item.trim();
-      });
-  }
+	ngOnInit() {
+	}
+	cleanKeywords(uncheckedUserInput: string) {
+		// this is a basic chop of the keywords into a list
+		// split on commas and semicolons
+		this.keywords = uncheckedUserInput
+			.split(',')
+			.map(item => {
+				// clean whitespace off of the ends
+				return item.trim();
+			});
+	}
 }

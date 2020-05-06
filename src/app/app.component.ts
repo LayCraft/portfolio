8d6377f1a7f99312a0655dc4b0313a73372779b0
LayCraft <curtis.laycraft@zoho.com>
2020-05-06 08:22:02 -0700

    update to github link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'This is an Angular App automatically deployed to Github pages using Travis CI. If there are some misplaced images please excuse them. It will get fixed when Curtis has a few moments to do work outside of work.';
-}
+export class AppComponent {}
